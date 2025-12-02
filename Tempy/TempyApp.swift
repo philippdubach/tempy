f9cc60a11fd421ffc09ba4d1d87e82356cc514d7
Philipp D. Dubach <phdubach@pm.me>
2025-12-02 10:55:34 +0100

    Initial Commit

---
+++ Tempy/TempyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempyApp.swift
+//  Tempy
+//
+//  Created by Philipp Dubach on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TempyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
